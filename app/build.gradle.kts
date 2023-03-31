f7e9cbc74a480434e2ce88c4d5668b6917d42fce
xXMRK888YTXx <aksehchikov@gmail.com>
2023-03-31 18:22:27 +0300

    [Feature] Integration Firebase Crashlytics

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     id ("com.android.application")
     id ("org.jetbrains.kotlin.android")
     id (Deps.Dagger.DaggerKaptPlugin)
+    id ("com.google.gms.google-services")
+    id ("com.google.firebase.crashlytics")
 }
 
 android {
@@ -73,4 +75,7 @@ dependencies {
     implementation(Deps.Compose.SystemUiController)
     implementation(Deps.AppCompat.appCompat)
     implementation(Deps.AppCompat.appCompatRes)
+    implementation (platform("com.google.firebase:firebase-bom:31.3.0"))
+    implementation ("com.google.firebase:firebase-crashlytics-ktx")
+    implementation ("com.google.firebase:firebase-analytics-ktx")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 buildscript {
-
+    dependencies {
+        classpath("com.google.gms:google-services:4.3.15")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.4")
+    }
 }// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id ("com.android.application") version "7.4.2" apply false
