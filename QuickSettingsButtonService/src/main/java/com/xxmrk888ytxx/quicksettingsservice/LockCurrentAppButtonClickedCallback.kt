4ab6412370f035a620d04bbebfba56d87d1ee2f1
xXMRK888YTXx <aksehchikov@gmail.com>
2023-04-01 13:13:20 +0300

    [Docs] Update docs

--- QuickSettingsButtonService/src/main/java/com/xxmrk888ytxx/quicksettingsservice/LockCurrentAppButtonClickedCallback.kt
+++ QuickSettingsButtonService/src/main/java/com/xxmrk888ytxx/quicksettingsservice/LockCurrentAppButtonClickedCallback.kt
@@ -1,5 +1,8 @@
 package com.xxmrk888ytxx.quicksettingsservice
 
+/** * [Ru] * Callback который будет вызван когда на кнопку быстрого доступа произведено нажатие */
+
+/** * [En] * Callback that will be called when the shortcut button is pressed */
 interface LockCurrentAppButtonClickedCallback {
 
     fun onClick()
